c5ec60f58f927f0e18510b1055f58c17027b4690
Kevalgor12 <kevalgor12@gmail.com>
2022-05-18 14:55:35 +0530

    code updated.

--- dbconnection/dbconnection.js
+++ dbconnection/dbconnection.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('blogapi', 'root', '', {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
 });
